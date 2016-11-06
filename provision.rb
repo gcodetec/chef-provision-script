926c614ffa7f2ab16c6966e36474dd0c0e4142cf
George Moura <gwmoura@gmail.com>
2016-11-06 18:38:48 -0300

    update script

--- provision.rb
+++ provision.rb
@@ -1,17 +1,17 @@
 require 'chef/provisioning'
 
 with_machine_options({
-                       :bootstrap_options => {
-                         :image_name => 'ubuntu-1404-trusty-v20161020',
-                         :zone_name => 'us-east1-b',
-                         :machine_type => 'f1-micro',
-                         # :key_name => 'id_rsa',  # this is the local name of your SSH private key.
-                         # You'll need to set up your ssh-key via: Home > Compute Engine > Metadata > SSH Keys and upload your public key
-                         :tags => ["fog", "http-server"],
-                         :metadata => {
-                             "newrelic-license-key" => ENV["NEWRELIC_LICENSE_KEY"]
-                             "startup-script-url" => "https://raw.githubusercontent.com/gcodetec/chef-provision-script/master/startup.sh"}
-                       }
-                     })
+                      :bootstrap_options => {
+                        :image_name => 'ubuntu-1404-trusty-v20161020',
+                        :zone_name => 'us-east1-b',
+                        :machine_type => 'f1-micro',
+                        :key_name => 'id_rsa',  # this is the local name of your SSH private key.
+                        # You'll need to set up your ssh-key via: Home > Compute Engine > Metadata > SSH Keys and upload your public key
+                        :tags => [],
+                        :metadata => {
+                            "newrelic-license-key" => ENV["NEWRELIC_LICENSE_KEY"],
+                            "startup-script-url" => "https://raw.githubusercontent.com/gcodetec/chef-provision-script/master/startup.sh"}
+                      }
+                    })
 
 machine "serverbychef"
--- startup.sh
+++ startup.sh
@@ -1,9 +1,13 @@
 #! /bin/bash
-export NEWRELIC_LICENSE_KEY=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/newrelic-license-key -H "Metadata-Flavor: Google")
-echo deb http://apt.newrelic.com/debian/ newrelic non-free >> /etc/apt/sources.list.d/newrelic.list
-wget -O- https://download.newrelic.com/548C16BF.gpg | apt-key add -
+CHEFDK_VERSION=0.19.6
+if [ "$NEWRELIC_LICENSE_KEY" == "" ]
+then
+    export NEWRELIC_LICENSE_KEY=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/newrelic-license-key -H "Metadata-Flavor: Google")
+fi
+echo $NEWRELIC_LICENSE_KEY > /tmp/newrelic_license_key
 sudo apt-get update
-sudo apt-get -y install curl git newrelic-sysmond
-nrsysmond-config --set license_key=$NEWRELIC_LICENSE_KEY
-/etc/init.d/newrelic-sysmond start
-curl https://omnitruck.chef.io/install.sh | sudo bash -s -- -P chefdk -c stable -v 0.16.28
+sudo apt-get -y install curl git
+curl https://omnitruck.chef.io/install.sh | sudo bash -s -- -P chefdk -c stable -v $CHEFDK_VERSION
+git clone https://github.com/gcodetec/tsuru-docker-node.git
+cd tsuru-docker-node && berks install && berks vendor /cookbooks && cd ../
+sudo chef-client --local-mode --runlist 'recipe[tsuru-docker-node]'
