1e31e92e661b06a241f78ba886e250f13ac36a19
George Moura <gwmoura@gmail.com>
2016-11-06 01:43:31 -0300

    add newrelic install

--- startup.sh
+++ startup.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 export NEWRELIC_LICENSE_KEY=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/newrelic-license-key -H "Metadata-Flavor: Google")
+echo deb http://apt.newrelic.com/debian/ newrelic non-free >> /etc/apt/sources.list.d/newrelic.list
+wget -O- https://download.newrelic.com/548C16BF.gpg | apt-key add -
 sudo apt-get update
-sudo apt-get -y install curl git
+sudo apt-get -y install curl git newrelic-sysmond
+nrsysmond-config --set license_key=$NEWRELIC_LICENSE_KEY
+/etc/init.d/newrelic-sysmond start
 curl https://omnitruck.chef.io/install.sh | sudo bash -s -- -P chefdk -c stable -v 0.16.28
-mkdir /tmp/chef-repo
-sudo chef-client --local-mode /tmp/chef-repo/webserver.rb
